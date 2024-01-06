308d4254b8de5df193f05a1332d89c26549cc57b
kingsleyibe <ibekingsley66@gmail.com>
2024-01-06 02:20:53 +0100

    Add page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Home from './components/home';
+import PageRoutes from '../src/routes/PageRoutes';
 // import './index.css'
 
 const App = () => {
   return (
     <>
-      <Home />
+      <PageRoutes />
     </>
   );
 };
