73749fbce0a53edb8fb5317087673e85e82f4480
kingsleyibe <ibekingsley66@gmail.com>
2024-01-11 13:58:18 +0100

    Add SearchTutor component

--- src/components/home/Search.tsx
+++ src/components/home/Search.tsx
@@ -1,20 +1,34 @@
+import { useState } from "react";
 import { faBookOpen, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SearchTutor from "./SearchTutor";
 
 const Search = () => {
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+        console.log(toggle);
+    }
   return (
-    <div className="text-center w-[50%] m-auto bg-[#FD9800] cursor p-3 rounded-[40px]">
+    <>
+    {!toggle ? 
+    <div onClick={handleToggle} className="text-center w-[50%] m-auto bg-[#FD9800] cursor p-3 rounded-[40px]">
         <div className="flex flex-row justify-between items-center">
             <div className="flex flex-row justify-center gap-2 items-center">
               <FontAwesomeIcon icon={faBookOpen} />
-              <button type="button">What do you want to learn?</button>
+              <button onClick={handleToggle} type="button">What do you want to learn?</button>
             </div>
             <div className="flex flex-row gap-2 items-center  bg-[#181842] py-[12px] px-[21px] rounded-[40px]">
               <FontAwesomeIcon icon={faSearch} />
-              <button type="button">Find a Tutor</button>
+              <button onClick={handleToggle} type="button">Find a Tutor</button>
             </div>
         </div>
     </div>
+    : (
+        <SearchTutor />
+    )}
+    </>
   );
 };
 
