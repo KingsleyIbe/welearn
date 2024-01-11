c7a100f6b38a224b957960b01b65e768514a4a89
kingsleyibe <ibekingsley66@gmail.com>
2024-01-11 12:15:44 +0100

    Fix bug with home page

---
+++ src/components/home/Caption1.tsx
@@ -0,0 +1,11 @@
+const Caption1 = () => {
+    return (
+        <div>
+            <div>
+                <h1>Find the Perfect Tutor</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Caption1;
