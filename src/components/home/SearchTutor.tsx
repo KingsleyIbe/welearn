a9eb3814e4c58f8ca44a5bfcb2043f6ee2b79cda
kingsleyibe <ibekingsley66@gmail.com>
2024-01-11 14:04:04 +0100

    Add SearchTutor component

--- src/components/home/SearchTutor.tsx
+++ src/components/home/SearchTutor.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SearchTutor = () => {
     return (
-        <div className="text-center w-[55%] m-auto bg-[#FD9800] cursor p-3 rounded-[40px]">
+        <div className="text-center w-[50%] m-auto bg-[#FD9800] cursor p-3 rounded-[40px]">
             <div className="flex flex-row justify-between items-center">
                 <div className="flex flex-row justify-center gap-5 items-center">
                     <div className="flex flex-row justify-center gap-2 items-center">
@@ -15,7 +15,7 @@ const SearchTutor = () => {
                         <input type="text" placeholder="By Location" className="s-input w-[100%] rounded-[8px] px-[10px] py-[8px]"/>
                     </div>
                 </div>
-                <div className="flex flex-row gap-2 items-center  bg-[#181842] py-[12px] px-[21px] rounded-[40px]">
+                <div className="flex flex-row gap-2 items-center  bg-[#181842] py-[12px] px-[18px] rounded-[40px]">
                     <FontAwesomeIcon icon={faSearch} />
                     <button type="button">Find a Tutor</button>
                 </div>
