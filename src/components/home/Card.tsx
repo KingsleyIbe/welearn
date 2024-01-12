4c184992740aee1d2e44c8de92972e56f14a5460
kingsleyibe <ibekingsley66@gmail.com>
2024-01-12 10:15:02 +0100

    Add styles to the cards

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -4,22 +4,22 @@ import { Data } from "../../db/tutorInfo";
 const Card = () => {
     return (
         <div className="bg-[#EAF3FF] m-10 p-10 rounded-[8px] text-[#000]">
-            <ul className="flex flex-row gap-2 w-[100%]">
+            <ul className="parent-flex-wrap gap-2 w-[100%]">
               {Data.map((item) => (
-                <li key={item?.id}>
-                    <div>
+                <li key={item?.id} className="child-flex-wrap">
+                    <div className="relative">
                         <img src={item?.image} alt="Tutor avatar" className="w-[400px] h-[400px]"/>
-                        <FontAwesomeIcon icon={item?.favouriteIcon}/>
-                        <p>{item?.name}</p>
-                        <div className="flex">
-                            <p>{item?.location}</p>
-                            <p>({item?.teachingMode})</p>
+                        <FontAwesomeIcon icon={item?.favouriteIcon} className="absolute top-5 right-5"/>
+                        <p className="absolute left-5 bottom-[70px] font-bold text-[30px] text-[#fff]">{item?.name}</p>
+                        <div className="flex absolute left-5 bottom-[50px] font-semibold text-[#FD9800]">
+                            <p>{item?.location }</p>
+                            <p> - ({item?.teachingMode})</p>
                         </div>
                     </div>
-                    <div>
+                    <div className="mx-2">
                         <div className="flex justify-between items-center">
                             <div className="flex gap-2 items-center">
-                                <FontAwesomeIcon icon={item?.ratingIcon} />
+                                <FontAwesomeIcon icon={item?.ratingIcon} className="text-[#FD9800]" />
                                 <p>{item?.rating} </p>
                                 <p>{item?.numberOfReviews} {item?.numberOfReviews > 0 ? 'reviews' : 'review'} </p>
                             </div>
--- src/db/tutorInfo.ts
+++ src/db/tutorInfo.ts
@@ -1,5 +1,10 @@
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faStarOfLife, faStarAndCrescent } from "@fortawesome/free-solid-svg-icons";
 import Image from '../assets/images/kings.jpg';
+import Image1 from '../assets/images/photo1.jpg';
+import Image2 from '../assets/images/photo2.jpg';
+import Image3 from '../assets/images/photo3.jpg';
+import Image4 from '../assets/images/photo4.jpg';
+import Image5 from '../assets/images/photo5.jpg';
 
 export const Data = [
     {
@@ -10,7 +15,7 @@ export const Data = [
         location: 'Lagos',
         teachingMode: ['face to face', 'webcam'],
         course: 'Web Development',
-        ratingIcon: faHeart,
+        ratingIcon: faStarOfLife,
         rating: 5,
         numberOfReviews: 10,
         status: 'confirmed',
@@ -22,7 +27,7 @@ export const Data = [
     {
         id: 2,
         favouriteIcon: faHeart,
-        image: Image,
+        image: Image1,
         name: 'Solution',
         location: 'Lagos',
         teachingMode: ['face to face'],
@@ -39,7 +44,58 @@ export const Data = [
     {
         id: 3,
         favouriteIcon: faHeart,
-        image: Image,
+        image: Image2,
+        name: 'Kingsley',
+        location: 'Aba',
+        teachingMode: ['face to face', 'webcam'],
+        course: 'Math',
+        ratingIcon: faStarAndCrescent,
+        rating: 5,
+        numberOfReviews: 10,
+        status: 'Experienced',
+        description: 'lorem lorem lorem sjjksa resaadnjd nsjnsjdcjjsnjcmdjnj',
+        price: 5000,
+        note: ''
+
+    },
+    {
+        id: 1,
+        favouriteIcon: faHeart,
+        image: Image3,
+        name: 'Kingsley',
+        location: 'Lagos',
+        teachingMode: ['face to face', 'webcam'],
+        course: 'Web Development',
+        ratingIcon: faHeart,
+        rating: 5,
+        numberOfReviews: 10,
+        status: 'confirmed',
+        description: 'lorem lorem lorem sjjksa resaadnjd nsjnsjdcjjsnjcmdjnj',
+        price: 3000,
+        note: 'First lesson is free'
+
+    },
+    {
+        id: 2,
+        favouriteIcon: faHeart,
+        image: Image4,
+        name: 'Solution',
+        location: 'Lagos',
+        teachingMode: ['face to face'],
+        course: 'Chemistry',
+        ratingIcon: faHeart,
+        rating: 0,
+        numberOfReviews: 0,
+        status: 'Super Tutor',
+        description: 'lorem lorem lorem sjjksa resaadnjd nsjnsjdcjjsnjcmdjnj',
+        price: 3000,
+        note: 'First lesson is free'
+
+    },
+    {
+        id: 3,
+        favouriteIcon: faHeart,
+        image: Image5,
         name: 'Kingsley',
         location: 'Aba',
         teachingMode: ['face to face', 'webcam'],
