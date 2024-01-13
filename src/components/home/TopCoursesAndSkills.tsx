c058a9cd1930c4748d0e4b10f12d96005162872d
kingsleyibe <ibekingsley66@gmail.com>
2024-01-13 17:07:11 +0100

    Add Top skills database

---
+++ src/components/home/TopCoursesAndSkills.tsx
@@ -0,0 +1,7 @@
+const TopCoursesAndSkills = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default TopCoursesAndSkills;
---
+++ src/db/TopSkills.ts
@@ -0,0 +1,47 @@
+export const Data = [
+    {
+        id: 1,
+        title: 'Academic Writing',
+        url: '/academic-writing-tutors'
+    }, 
+    {
+        id: 2,
+        title: 'Analog Electronics',
+        url: 'analog-electronics-tutors'
+    },
+    {
+        id: 3,
+        title: 'BioChemistry',
+        url: '/bioChemistry-tutors'
+    },
+    {
+        id: 4,
+        title: 'C/C++',
+        url: '/c_c-plus-plus-tutors'
+    }, 
+    {
+        id: 5,
+        title: 'Commerce',
+        url: '/commerce-tutors'
+    },
+    {
+        id: 6,
+        title: 'Computer Science',
+        url: '/Computer Science-tutors'
+    },
+    {
+        id: 7,
+        title: '.net',
+        url: '/dot_net-tutors'
+    }, 
+    {
+        id: 8,
+        title: 'English',
+        url: '/english-tutors'
+    },
+    {
+        id: 9,
+        title: 'Computer Science',
+        url: '/Computer Science-tutor'
+    },
+]
