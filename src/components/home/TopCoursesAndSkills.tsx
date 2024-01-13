cb113fe2ce7fc3de4ff857e859e448f7e0d116cf
kingsleyibe <ibekingsley66@gmail.com>
2024-01-13 17:45:55 +0100

    Add TopCoursesAndSkills component

--- src/components/home/TopCoursesAndSkills.tsx
+++ src/components/home/TopCoursesAndSkills.tsx
@@ -1,16 +1,16 @@
-import { Data } from "@/db/TopSkills";
+import { Data } from "../../db/TopSkills";
 import { Link } from "react-router-dom";
 
 const TopCoursesAndSkills = () => {
     return (
         <div>
-            <div>
+            <div className="text-center font-bold text-[24px]">
                 <h1>Top Courses And Skills</h1>
             </div>
-            <div>
-                <ul>
+            <div className="p-10">
+                <ul className="top-skills">
                     {Data?.map((item) => (
-                        <li key={item?.id}><Link to={item?.url}>{item?.title}</Link></li>
+                        <li className="child-top-skills" key={item?.id}><Link to={item?.url}>{item?.title}</Link></li>
                     ))}
                 </ul>
             </div>
