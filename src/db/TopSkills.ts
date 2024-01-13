43e48d63055209387d05d8822ee8dedb1993b70e
kingsleyibe <ibekingsley66@gmail.com>
2024-01-13 17:22:59 +0100

    Update Top skills database

--- src/db/TopSkills.ts
+++ src/db/TopSkills.ts
@@ -27,7 +27,7 @@ export const Data = [
     {
         id: 6,
         title: 'Computer Science',
-        url: '/Computer Science-tutors'
+        url: '/computer-Science-tutors'
     },
     {
         id: 7,
@@ -41,7 +41,112 @@ export const Data = [
     },
     {
         id: 9,
-        title: 'Computer Science',
-        url: '/Computer Science-tutor'
+        title: 'French',
+        url: '/french-tutors'
+    },
+    {
+        id: 10,
+        title: 'HTML',
+        url: '/html-tutors'
+    }, 
+    {
+        id: 11,
+        title: 'Jquery and JavaScript',
+        url: '/jquery-and-javaScript-tutors'
+    },
+    {
+        id: 12,
+        title: 'Microbiology',
+        url: '/microbiology-tutors'
+    },
+    {
+        id: 13,
+        title: 'Political Science',
+        url: '/political-science-tutors'
+    }, 
+    {
+        id: 14,
+        title: 'R',
+        url: '/r-tutors'
+    },
+    {
+        id: 15,
+        title: 'Statistics',
+        url: '/microbiology-tutors'
+    },
+    {
+        id: 16,
+        title: 'Accountancy',
+        url: '/accountancy-tutors'
+    }, 
+    {
+        id: 17,
+        title: 'Art and Craft',
+        url: 'art-and-craft-tutors'
+    },
+    {
+        id: 18,
+        title: 'Biology',
+        url: '/biology-tutors'
+    },
+    {
+        id: 19,
+        title: 'C#',
+        url: '/c_sharp-tutors'
+    }, 
+    {
+        id: 20,
+        title: 'Communication Skills',
+        url: '/communication-skills-tutors'
+    },
+    {
+        id: 21,
+        title: 'Control Systems',
+        url: '/control-systems-tutors'
+    },
+    {
+        id: 22,
+        title: 'Economics',
+        url: '/economics-tutors'
+    }, 
+    {
+        id: 23,
+        title: 'Environmental Science',
+        url: '/environmental-science-tutors'
+    },
+    {
+        id: 24,
+        title: 'Geography',
+        url: '/geography-tutors'
+    },
+    {
+        id: 25,
+        title: 'IELTS',
+        url: '/ielts-tutors'
+    }, 
+    {
+        id: 26,
+        title: 'Law',
+        url: '/law-tutors'
+    },
+    {
+        id: 27,
+        title: 'Music',
+        url: '/music-tutors'
+    },
+    {
+        id: 28,
+        title: 'Programming',
+        url: '/programming-tutors'
+    }, 
+    {
+        id: 29,
+        title: 'Science',
+        url: '/science-tutors'
+    },
+    {
+        id: 30,
+        title: 'Statistics',
+        url: '/microbiology-tutors'
     },
 ]
