50e97417d003ab45944d0ff089eb134c88bb0bbe
kingsleyibe <ibekingsley66@gmail.com>
2024-01-15 12:21:56 +0100

    Update page route

---
+++ src/components/dashboard/myMessages/index.tsx
@@ -0,0 +1,7 @@
+const index = () => {
+    return (
+        <div>My messages coming soon...</div>
+    );
+};
+
+export default index;
---
+++ src/components/dashboard/reviews/index.tsx
@@ -0,0 +1,7 @@
+const index = () => {
+    return (
+        <div>Reviews coming soon...</div>
+    );
+};
+
+export default index;
