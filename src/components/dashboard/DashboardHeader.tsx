93c136460c7c8940c301d6d110cf9600bcd1deab
kingsleyibe <ibekingsley66@gmail.com>
2024-01-15 12:05:23 +0100

    Add dashboard header component

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const DashboardHeader = () => {
     return (
         <div className="mt-[120px] text-[#181842]">
-            <ul className="flex flex-row gap-10 px-10 py-2 bg-[#FD9800] font-bold">
+            <ul className="flex flex-row gap-10 justify-center px-10 py-2 bg-[#FD9800] font-bold">
                 {Data?.map((item) => (
                     <li key={item?.id}><Link to={item?.href}>{item?.title}</Link></li>
                 ))}
