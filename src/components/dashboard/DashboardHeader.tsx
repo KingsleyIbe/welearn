49993099f2ebea8849892a37c0b71daf124b0501
kingsleyibe <ibekingsley66@gmail.com>
2024-01-15 11:09:11 +0100

    Add dashboard header component

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,7 +1,7 @@
 const DashboardHeader = () => {
     return (
-        <div className="mt-[120px]">
-            <ul className="flex flex-row gap-10 p-10">
+        <div className="mt-[120px] text-[#181842]">
+            <ul className="flex flex-row gap-10 p-10 bg-[#FD9800] font-bold">
                 <li><button type="button">Dashboard</button></li>
                 <li><button type="button">My Messages</button></li>
                 <li><button type="button">My Ads</button></li>
