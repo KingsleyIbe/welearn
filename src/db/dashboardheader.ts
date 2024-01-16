3a8ceb2ce4c7ac393725450ec6e8e337d0238bf0
kingsleyibe <ibekingsley66@gmail.com>
2024-01-16 11:43:40 +0100

    Update dashboard header data

--- src/db/dashboardheader.ts
+++ src/db/dashboardheader.ts
@@ -1,32 +1,47 @@
 export const Data = [
     {
         id: 1,
-        title: 'Dashboard',
+        title: 'My Account',
         href: '/dashboard'
     },
     {
         id: 2,
+        title: 'Profile',
+        href: '/dashboard/profile'
+    },
+    {
+        id: 3,
         title: 'My Messages',
         href: '/dashboard/my-messages'
     },
     {
-        id: 3,
+        id: 4,
         title: 'My Ads',
         href: '/dashboard/my-ads'
     },
     {
-        id: 4,
+        id: 5,
         title: 'Reviews',
         href: '/dashboard/reviews'
     },
     {
-        id: 5,
-        title: 'My Account',
-        href: '/dashboard/my-account'
+        id: 6,
+        title: 'Scheduled Lessons',
+        href: '/dashboard/scheduled-lessons'
     },
     {
-        id: 6,
+        id: 7,
+        title: 'My Students',
+        href: '/dashboard/my-students'
+    },
+    {
+        id: 8,
         title: 'My Subscriptions',
         href: '/dashboard/my-subscriptions'
+    },
+    {
+        id: 9,
+        title: 'Settings',
+        href: '/dashboard/settings'
     },
 ]
