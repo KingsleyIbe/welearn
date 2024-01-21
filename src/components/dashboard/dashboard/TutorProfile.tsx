67b03d7556fc747caa3730ab3af6ffb627da4f28
kingsleyibe <ibekingsley66@gmail.com>
2024-01-21 23:50:21 +0100

    Updated dashboard page

--- src/components/dashboard/dashboard/TutorProfile.tsx
+++ src/components/dashboard/dashboard/TutorProfile.tsx
@@ -1,13 +1,7 @@
-import Footer from "../../../components/common/Footer";
-import Heading from "../../../components/common/Heading";
-import DashBoardPage from "../../../pages/DashBordPage";
-
 const TutorProfile = () => {
     return (
         <div>
-            <Heading />
-            <DashBoardPage />
-            <Footer />
+            new
         </div>
     );
 };
---
+++ src/components/dashboard/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../../components/common/Footer";
+import Heading from "../../../components/common/Heading";
+import DashBoardPage from "../../../pages/DashBordPage";
+
+const index = () => {
+    return (
+        <div>
+            <Heading />
+            <DashBoardPage />
+            <Footer />
+        </div>
+    );
+};
+
+export default index;
