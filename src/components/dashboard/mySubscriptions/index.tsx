adfaee0acace378ed6eebd25e336aef20fff7d3d
kingsleyibe <ibekingsley66@gmail.com>
2024-01-21 23:32:37 +0100

    Updated dashboard page

--- src/components/dashboard/mySubscriptions/index.tsx
+++ src/components/dashboard/mySubscriptions/index.tsx
@@ -1,6 +1,12 @@
+import Heading from "../../../components/common/Heading";
+import DashBoardPage from "../../../pages/DashBordPage";
+
 const index = () => {
     return (
-        <div>My subscription coming soon...</div>
+        <div>
+            <Heading />
+            <DashBoardPage />
+        </div>
     );
 };
 
