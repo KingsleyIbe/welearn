f3c8f74afd0db889b047c6419ae4049aaa31849a
kingsleyibe <ibekingsley66@gmail.com>
2024-01-23 04:08:49 +0100

    Update page route

--- src/components/dashboard/myAds/index.tsx
+++ src/components/dashboard/myAds/index.tsx
@@ -2,14 +2,12 @@ import DashBoardPage from "@/pages/DashBordPage";
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
 import DashboardHeader from "../DashboardHeader";
-// import DashBoardPage from "../../../pages/DashBordPage";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
-            {/* <DashBoardPage /> */}
             <Footer />
         </div>
     );
--- src/components/dashboard/myStudents/index.tsx
+++ src/components/dashboard/myStudents/index.tsx
@@ -1,14 +1,12 @@
 import Footer from "../../common/Footer";
 import Heading from "../../common/Heading";
 import DashboardHeader from "../DashboardHeader";
-// import DashBoardPage from "../../../pages/DashBordPage";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
-            {/* <DashBoardPage /> */}
             <Footer />
         </div>
     );
