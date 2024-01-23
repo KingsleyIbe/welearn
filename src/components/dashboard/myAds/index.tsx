a8951c467fd2e95bb31d252dc2a784866fa4b607
kingsleyibe <ibekingsley66@gmail.com>
2024-01-23 03:43:56 +0100

    Fix bug with dashboard

--- src/components/dashboard/myAds/index.tsx
+++ src/components/dashboard/myAds/index.tsx
@@ -1,11 +1,14 @@
+import DashBoardPage from "@/pages/DashBordPage";
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
+import DashboardHeader from "../DashboardHeader";
 // import DashBoardPage from "../../../pages/DashBordPage";
 
 const index = () => {
     return (
         <div>
             <Heading />
+            <DashboardHeader />
             {/* <DashBoardPage /> */}
             <Footer />
         </div>
--- src/components/dashboard/mySubscriptions/index.tsx
+++ src/components/dashboard/mySubscriptions/index.tsx
@@ -1,11 +1,13 @@
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
+import DashboardHeader from "../DashboardHeader";
 // import DashBoardPage from "../../../pages/DashBordPage";
 
 const index = () => {
     return (
         <div>
             <Heading />
+            <DashboardHeader />
             {/* <DashBoardPage /> */}
             <Footer />
         </div>
