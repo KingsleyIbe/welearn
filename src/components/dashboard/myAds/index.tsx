e22f554dbfa10a6d94892eb7c3fc2221280f8a21
kingsleyibe <ibekingsley66@gmail.com>
2024-01-24 10:05:46 +0100

    Update the my ads page

--- src/components/dashboard/myAds/index.tsx
+++ src/components/dashboard/myAds/index.tsx
@@ -2,12 +2,14 @@ import DashBoardPage from "@/pages/DashBordPage";
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
 import DashboardHeader from "../DashboardHeader";
+import NoAds from "./NoAds";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
+            <NoAds />
             <Footer />
         </div>
     );
