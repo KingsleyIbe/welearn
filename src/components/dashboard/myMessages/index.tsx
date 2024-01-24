130caaafb30bb23791ac22240477c5f42c4367b3
kingsleyibe <ibekingsley66@gmail.com>
2024-01-24 10:12:21 +0100

    Update the my ads page

--- src/components/dashboard/myMessages/index.tsx
+++ src/components/dashboard/myMessages/index.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
 import DashboardHeader from "../DashboardHeader";
+import NoMessages from "./NoMessages";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
+            <NoMessages />
             <Footer />
         </div>
     );
