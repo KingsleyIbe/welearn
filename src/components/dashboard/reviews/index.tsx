34b5aa8d678333ba79f22bfc646c0ee7696ddd14
kingsleyibe <ibekingsley66@gmail.com>
2024-01-24 10:01:54 +0100

    Update the my student page

--- src/components/dashboard/reviews/index.tsx
+++ src/components/dashboard/reviews/index.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
 import DashboardHeader from "../DashboardHeader";
+import NoReviews from "./NoReviews";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
+            <NoReviews />
             <Footer />
         </div>
     );
