cc6cc6df4c410e127431a8a2ddcdf680a141ec1b
kingsleyibe <ibekingsley66@gmail.com>
2024-01-24 10:20:32 +0100

    Update the my ads page

---
+++ src/components/dashboard/scheduledLessons/NoScheduledLessons.tsx
@@ -0,0 +1,11 @@
+const NoScheduledLessons = () => {
+    return (
+        <div className="text-center font-bold text-[30px] my-[200px]">
+            <h1>
+                You do not have any scheduled lessons yet!
+            </h1>
+        </div>
+    );
+};
+
+export default NoScheduledLessons;
--- src/components/dashboard/scheduledLessons/index.tsx
+++ src/components/dashboard/scheduledLessons/index.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
 import DashboardHeader from "../DashboardHeader";
+import NoScheduledLessons from "./NoScheduledLessons";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
+            <NoScheduledLessons />
             <Footer />
         </div>
     );
