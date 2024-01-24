cb5957f8f70859cd6462742441cb43babb67427e
kingsleyibe <ibekingsley66@gmail.com>
2024-01-24 09:56:38 +0100

    Update the my student page

---
+++ src/components/dashboard/myStudents/NoStudent.tsx
@@ -0,0 +1,11 @@
+const NoStudent = () => {
+    return (
+        <div className="text-center font-bold text-[30px] my-[200px]">
+            <h1>
+                You do not have any student yet.
+            </h1>
+        </div>
+    );
+};
+
+export default NoStudent;
--- src/components/dashboard/myStudents/index.tsx
+++ src/components/dashboard/myStudents/index.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../common/Footer";
 import Heading from "../../common/Heading";
 import DashboardHeader from "../DashboardHeader";
+import NoStudent from "./NoStudent";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
+            <NoStudent />
             <Footer />
         </div>
     );
