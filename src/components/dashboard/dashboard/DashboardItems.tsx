7aa22c2043de535ae72ed3de0c64b5e77831af2c
kingsleyibe <ibekingsley66@gmail.com>
2024-01-27 19:55:21 +0100

    Update dashboard page

--- src/components/dashboard/dashboard/DashboardItems.tsx
+++ src/components/dashboard/dashboard/DashboardItems.tsx
@@ -2,7 +2,21 @@ const DashboardItems = () => {
     return (
         <div className="text-center font-bold text-[30px] my-[200px] px-10">
             <div className="flex flex-row gap-2">
-                <div className="bg-[#ccc]">New</div>
+                <div className="bg-[#ccc]">
+                    <div>
+                        <h3>Congratulations! Your profile has been approved</h3>
+                        <p>Your profile is now public! You are now ready to teach on Weteach.</p>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><button type="button">Basic Info</button></li>
+                            <li><button type="button">Subjects</button></li>
+                            <li><button type="button">Profile Description</button></li>
+                            <li><button type="button">Education and experience</button></li>
+                            <li><button type="button">Education and experience</button></li>
+                        </ul>
+                    </nav>
+                </div>
                 <div className="bg-[#ddd]">Old</div>
             </div>
         </div>
