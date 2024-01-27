dfd7ce86df176c5ef42c850cec6700464f2a656a
kingsleyibe <ibekingsley66@gmail.com>
2024-01-27 19:42:26 +0100

    Update dashboard page

---
+++ src/components/dashboard/dashboard/DashboardItems.tsx
@@ -0,0 +1,12 @@
+const DashboardItems = () => {
+    return (
+        <div>
+            <div className="flex flex-row gap">
+                <div className="bg-[#ccc]">New</div>
+                <div className="bg-[#ddd]">Old</div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardItems;
--- src/components/dashboard/dashboard/index.tsx
+++ src/components/dashboard/dashboard/index.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../../components/common/Footer";
 import Heading from "../../../components/common/Heading";
 import DashboardHeader from "../DashboardHeader";
+import DashboardItems from "./DashboardItems";
 
 const index = () => {
     return (
         <div>
             <Heading />
             <DashboardHeader />
+            <DashboardItems />
             <Footer />
         </div>
     );
