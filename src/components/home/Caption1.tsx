aecbc261d5846026dd4933f2244a20a8565529c6
kingsleyibe <ibekingsley66@gmail.com>
2024-01-27 16:01:41 +0100

    Update home page

--- src/components/home/Caption1.tsx
+++ src/components/home/Caption1.tsx
@@ -1,6 +1,6 @@
 const Caption1 = () => {
     return (
-        <div className="mb-10 mt-[100px]">
+        <div className="mb-10 mt-[120px]">
             <div className="text-center text-[80px] font-bold">
                 <h1>Find the Perfect Tutor</h1>
             </div>
