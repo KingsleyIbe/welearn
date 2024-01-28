2c222711a2ae29b529cfb1d314abd020259b0751
kingsleyibe <ibekingsley66@gmail.com>
2024-01-28 05:40:41 +0100

    Update dashboard page

--- src/components/dashboard/dashboard/UserData.tsx
+++ src/components/dashboard/dashboard/UserData.tsx
@@ -3,7 +3,7 @@ const UserData = () => {
         <div>
             <form action="">
                 <h2 className="font-bold my-10 text-[18px]">Change Name</h2>
-                <div className="w-[100%] m-auto flex flex-col items-end gap-1">
+                <div className="w-[100%] m-auto flex flex-col items-start gap-1">
                     <div className="flex justify-center flex-row gap-5 my-2 items-center p-2">
                         <label htmlFor="confirm_password" className="items-end">First Name:</label>
                         <input type="text" placeholder="First Name" className="register-input-bolder p-2 w-[300px]" />
@@ -13,8 +13,17 @@ const UserData = () => {
                         <input type="text" placeholder="Last Name" className="register-input-bolder p-2 w-[300px]" />
                     </div>
                     <div className="flex justify-center flex-row gap-5 my-2 items-center p-2">
-                        <label htmlFor="confirm_password" className="">Date of Birth</label>
+                        <label htmlFor="confirm_password" className="">Date of Birth:</label>
                         <input type="date" placeholder="mm/dd/yy" className="register-input-bolder p-2 w-[300px]" />
+                    </div>
+                    <div className="flex justify-center flex-ro gap-5 my-2 items-center p-2">
+                        <label htmlFor="confirm_password" className="">Gender:</label>
+                        <select className="register-input-bolder p-2 w-[300px]">
+                            <option>Select your gender</option>
+                            <option>Male</option>
+                            <option>Female</option>
+                        </select>
+
                     </div>
                 </div>
                 <div className="flex flex-col items-end justify-cente p-2">
