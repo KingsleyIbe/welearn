7187240b9d92b1140ca6c5134077f01367aff60b
kingsleyibe <ibekingsley66@gmail.com>
2024-01-28 04:55:01 +0100

    Update dashboard page

--- src/components/dashboard/dashboard/BasicInfo.tsx
+++ src/components/dashboard/dashboard/BasicInfo.tsx
@@ -1,3 +1,4 @@
+import ProfilePhoto from '../../../assets/images/photo1.jpg';
 const BasicInfo = () => {
     return (
         <div className="bg-[#fff] text-[#181842] p-10 rounded-[8px]">
@@ -7,6 +8,28 @@ const BasicInfo = () => {
                 <p className="flex flex-col text-end text-[#f00]">Required fields: *</p>
             </div>
             <hr />
+            <div>
+                <div className="flex flex-col items-center mt-10">
+                    <div className="flex flex-row gap-4 items-center">
+                        <div>
+                            <img src={ProfilePhoto} alt="Profile avatar" className="rounded-[100%] w-[120px] h-[120px]" />
+
+                        </div>
+                        <div className="flex flex-col gap-4 items-start">
+                            <button type="button" className="upload-photo-btn">Upload a photo</button>
+                            <button type="button" className="upload-photo-btn">Take a photo</button>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Tips for an incredible photo:</h3>
+                        <ul>
+                            <li>Smile and look at the camera</li>
+                            <li>Centre your head and shoulders</li>
+                            <li>Make sure your picture is centred and upright</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
