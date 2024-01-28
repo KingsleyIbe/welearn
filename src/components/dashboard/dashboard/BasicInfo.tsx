07b92883e106aece03a4190535a6c4575d074b5a
kingsleyibe <ibekingsley66@gmail.com>
2024-01-28 14:23:09 +0100

    Add Readmore button

--- src/components/dashboard/dashboard/BasicInfo.tsx
+++ src/components/dashboard/dashboard/BasicInfo.tsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import ProfilePhoto from '../../../assets/images/photo1.jpg';
 import UserData from './UserData';
 const BasicInfo = () => {
+    const [isReadMore, setIsReadMore] = useState(false);
+
+    const handleReadMore = () => {
+        setIsReadMore(!isReadMore);
+    };
     return (
         <div className="bg-[#fff] text-[#181842] p-10 rounded-[8px]">
             <div className="mb-5">
@@ -26,12 +32,19 @@ const BasicInfo = () => {
                         <ul className="flex flex-col ml-10 gap-2 text-start justify-center photo-tips">
                             <li>Smile and look at the camera.</li>
                             <li>Centre your head and shoulders.</li>
-                            <li>Make sure your picture is centred and upright.</li>
-                            <li>Use neutral lighting and backgrounds.</li>
-                            <li>Your face and eyes should be fully visible (unless you have cultural reasons that contradict this).</li>
-                            <li>Avoid logos or contact information.</li>
-                            <li>You should be the only person in the photo.</li>
+                            <li className="opacity-[0.2]">Make sure your picture is centred and upright.</li>
+                            {isReadMore && (
+                                <ul>
+                                <li>Use neutral lighting and backgrounds.</li>
+                                <li>Your face and eyes should be fully visible (unless you have cultural reasons that contradict this).</li>
+                                <li>Avoid logos or contact information.</li>
+                                <li>You should be the only person in the photo.</li>
+                                </ul>
+                            )}
                         </ul>
+                        <div className="flex flex-col items-end text-center text-[#FD9800] font-bold">
+                            <button type="button" onClick={handleReadMore}>{isReadMore ? 'Read More' : 'Read Less'}</button>
+                        </div>
                     </div>
                     <UserData />
                 </div>
