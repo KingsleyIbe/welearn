c1916ad80c418c3c2cd7304811268b632d14de6e
kingsleyibe <ibekingsley66@gmail.com>
2024-01-30 15:45:51 +0100

    Update My Profile Description on the dashboard

--- src/components/dashboard/dashboard/MyProfileDesc.tsx
+++ src/components/dashboard/dashboard/MyProfileDesc.tsx
@@ -10,7 +10,7 @@ const MyProfileDesc = () => {
     return (
         <div className="bg-[#fff] text-[#181842] p-10 rounded-[8px]">
             <div className="mb-5">
-                <h1 className="text-[30px] font-bold mb-5">Your Personal Information</h1>
+                <h1 className="text-[30px] font-bold mb-5">Profile description</h1>
                 <p>In order for us to validate your profile, please fill in the required fields. Remember that the more quality information you provide, the more chances you have to attract new students.</p>
                 <p className="flex flex-col text-end text-[#f00]">Required fields: *</p>
             </div>
