67a8477390bdd431d075542e9ef13358b46eade6
kingsleyibe <ibekingsley66@gmail.com>
2024-01-31 21:52:14 +0100

    Update My Profile Description on the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import LinkRoutes from './routes/LinkRoutes';
 
 const App = () => {
   return (
-    // <BrowserRouter>
     <div>
-      {/* <PageRoutes /> */}
       <LinkRoutes />
       </div>
-      // </BrowserRouter>
   );
 };
 
