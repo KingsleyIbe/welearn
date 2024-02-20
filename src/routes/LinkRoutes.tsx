421142ea23ff36cdf101aae6cef29d008a79704f
kingsleyibe <ibekingsley66@gmail.com>
2024-02-20 01:54:23 +0100

    Implement lazy load

--- src/routes/LinkRoutes.tsx
+++ src/routes/LinkRoutes.tsx
@@ -5,13 +5,13 @@ const LoginPage = lazy(() => import('../pages/LoginPage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage'));
 const DashBoardPage = lazy(() => import('../pages/DashBordPage'));
 const ReviewsPage = lazy(() => import('../pages/ReviewsPages'));
-const ReviewsPage = lazy(() => import('../pages/ReviewsPages'));
+const MyAdsPage = lazy(() => import('../pages/MyAdsPage'));
   // import Home from '../pages/HomePage';
 // import LoginPage from '../pages/LoginPage';
 // import RegisterPage from '../pages/RegisterPage';
 // import DashBoardPage from '../pages/DashBordPage';
 // import ReviewsPage from '../pages/ReviewsPages';
-import MyAdsPage from '../pages/MyAdsPage';
+// import MyAdsPage from '../pages/MyAdsPage';
 import MyAccountPage from '../pages/MyAccountPage';
 import MySubscriptionsPage from '../pages/MySubscriptionsPage';
 import MyMessagesPage from '../pages/MyMessagesPage';
@@ -28,7 +28,7 @@ const LinkRoutes = () => {
         <Route path="/register" element={<Suspense fallback={<>Loading...</>}><RegisterPage /></Suspense>} />
         <Route path="/dashboard" element={<Suspense fallback={<>Loading...</>}><DashBoardPage /></Suspense>} />
         <Route path="/dashboard/reviews" element={<Suspense fallback={<>Loading...</>}><ReviewsPage /></Suspense>} />
-        <Route path="/dashboard/my-ads" element={<MyAdsPage />} />
+        <Route path="/dashboard/my-ads" element={<Suspense fallback={<>Loading...</>}><MyAdsPage /></Suspense>} />
         <Route path="/dashboard/my-account" element={<MyAccountPage />} />
         <Route path="/dashboard/my-messages" element={<MyMessagesPage />} />
         <Route path="/dashboard/my-subscriptions" element={<MySubscriptionsPage />} />
